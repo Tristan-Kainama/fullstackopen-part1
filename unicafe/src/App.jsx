b9122340682e47ee00e583b2613478a430ad4553
baveryk <io.kainama@gmail.com>
2026-07-28 23:44:47 +0700

    Finished exercise 1.6 (real)

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -6,9 +6,21 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodButton = () => {setGood(good+1)};
+  const handleNeutralButton = () => {setNeutral(neutral+1)};
+  const handleBadButton = () => {setBad(bad+1)};
+
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <button onClick={handleGoodButton}>good</button>
+      <button onClick={handleNeutralButton}>neutral</button>
+      <button onClick={handleBadButton}>bad</button>
+
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
