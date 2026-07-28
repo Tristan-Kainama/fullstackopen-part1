b56fd014e92f7827c74a7daebc4a803fe9af0753
baveryk <io.kainama@gmail.com>
2026-07-29 00:12:06 +0700

    Finished exercise 1.7

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -5,10 +5,50 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
-  const handleGoodButton = () => {setGood(good+1)};
-  const handleNeutralButton = () => {setNeutral(neutral+1)};
-  const handleBadButton = () => {setBad(bad+1)};
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  const countAverage = (newGood, newBad, newAll) => {
+    const totalScore = newGood - newBad;
+    const newAverage = totalScore/newAll;
+
+    setAverage(newAverage);
+  }
+
+  const countPositive = (newGood, newAll) => {
+    setPositive(newGood / newAll);
+  }
+
+  const handleGoodButton = () => {
+    const newGood = good+1
+    const newAll = newGood+bad+neutral
+
+    setGood(newGood)
+    setAll(newAll)
+    countAverage(newGood, bad, newAll);
+    countPositive(newGood, newAll);
+  };
+
+  const handleNeutralButton = () => {
+    const newNeutral = neutral+1;
+    const newAll = good+newNeutral+bad;
+
+    setNeutral(newNeutral);
+    setAll(newAll);
+    countAverage(good, bad, newAll);
+    countPositive(good, newAll);
+  };
+  const handleBadButton = () => {
+    const newBad = bad+1;
+    const newAll = good+neutral+newBad;
+    
+    setBad(newBad);
+    setAll(newAll);
+    countAverage(good, newBad, newAll);
+    countPositive(good, newAll);
+  };
 
   return (
     <div>
@@ -21,6 +61,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
